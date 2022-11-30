3cd8f0d76501c856210450f1c6781634e78ba297
Matthew Lewis <matthew.knight.lewis@gmail.com>
2022-11-30 10:40:56 -0500

    new parses

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 const (
@@ -23,9 +24,9 @@ func main() {
 		os.Exit(1)
 	}
 	defer server.Close()
+
 	fmt.Println("Listening on " + host + ":" + port)
 	fmt.Println("Waiting for client...")
-
 	for {
 		connection, err := server.Accept()
 		if err != nil {
@@ -33,17 +34,41 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Println("client connected")
-		go processClient(connection)
+		go threadedClientConnectionHandler(connection)
 	}
 }
 
-func processClient(connection net.Conn) {
+func threadedClientConnectionHandler(connection net.Conn) {
 	buffer := make([]byte, 1024)
 	mLen, err := connection.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
 	fmt.Println("Received: ", string(buffer[:mLen]))
+
+	message := string(buffer[:mLen])
+
+	// AP00 = Connection
+	if strings.Contains(message, "AP00") {
+		parseAP00(string(buffer[:mLen]))
+	} else if strings.Contains(message, "AP01") {
+		parseAP01(string(buffer[:mLen]))
+	} else if strings.Contains(message, "AP10") {
+		parseAP10(string(buffer[:mLen]))
+	}
+
 	_, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
-	connection.Close()
+	//connection.Close()
+}
+
+func parseAP00(msg string) string {
+	return msg
+}
+
+func parseAP01(msg string) string {
+	return msg
+}
+
+func parseAP10(msg string) string {
+	return msg
 }
