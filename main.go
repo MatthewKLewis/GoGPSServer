e33e2189beb1fc71e763a16e7bd0b530521abc8e
Matthew Lewis <matthew.knight.lewis@gmail.com>
2022-11-30 13:01:02 -0500

    new parsers, IMEI gathering

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 )
 
 const (
-	host           = "localhost"
+	host           = "172.31.51.82" // 127.0.0.1 // 172.31.51.82
 	port           = "8000"
 	connectionType = "tcp"
 	locationRoute  = "http://3.212.201.170:802/XpertRestApi/api/location_data"
 	alertRoute     = "http://3.212.201.170:802/XpertRestApi/api/alert_data"
 )
 
+var numberOfConnections = 0
+
 func main() {
+	connectionMap := make(map[string]int)
 
 	fmt.Println("Server Running...")
 	server, err := net.Listen(connectionType, host+":"+port)
@@ -25,38 +28,58 @@ func main() {
 	}
 	defer server.Close()
 
-	fmt.Println("Listening on " + host + ":" + port)
-	fmt.Println("Waiting for client...")
+	fmt.Println("Listening on", host, ":", port)
 	for {
 		connection, err := server.Accept()
 		if err != nil {
 			fmt.Println("Error accepting: ", err.Error())
 			os.Exit(1)
 		}
-		fmt.Println("client connected")
+		numberOfConnections++
+		connectionMap[connection.RemoteAddr().String()] = numberOfConnections
+		//fmt.Println(connectionMap)
 		go threadedClientConnectionHandler(connection)
 	}
 }
 
 func threadedClientConnectionHandler(connection net.Conn) {
-	buffer := make([]byte, 1024)
-	mLen, err := connection.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+	for {
+		buffer := make([]byte, 1024)
+
+		// Waits here for next message...
+		mLen, err := connection.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+			connection.Close()
+			return
+		}
+
+		// Handles Message
+		fmt.Println("Received from:", connection.RemoteAddr().String())
+		message := string(buffer[:mLen])
+		if strings.Contains(message, "AP00") { // AP00 = Connection
+			fmt.Println("-- AP00")
+			fmt.Println("-- IMEI:", getIMEIFromAP00(string(buffer[:mLen])))
+
+		} else if strings.Contains(message, "AP01") { // AP01 = Location?
+			fmt.Println("-- AP01")
+			fmt.Println("--", getJSONFromAP01(string(buffer[:mLen])))
+
+		} else if strings.Contains(message, "AP03") { // AP03 = Heartbeat
+			fmt.Println("-- AP03")
+			//fmt.Println("--", getJSONFromAP01(string(buffer[:mLen])))
+
+		} else if strings.Contains(message, "AP10") { // AP10 = Alert?
+			fmt.Println("-- AP10")
+			fmt.Println("--", getJSONFromAP10(string(buffer[:mLen])))
+
+		} else if strings.Contains(message, "LK") { // LK = Other Tag?
+			fmt.Println("-- LK")
+			fmt.Println("-- IMEI:", getIMEIFromLK(string(buffer[:mLen])))
+
+		} else {
+			//fmt.Println("-- MESSAGE OTHER THAN AP 00, 01, 10")
+
+		}
 	}
-	fmt.Println("Received: ", string(buffer[:mLen]), " from: ", connection.LocalAddr().String())
-
-	message := string(buffer[:mLen])
-
-	if strings.Contains(message, "AP00") { // AP00 = Connection
-		fmt.Print(parseAP00(string(buffer[:mLen])))
-
-	} else if strings.Contains(message, "AP01") { // AP01 = Location?
-		fmt.Print(parseAP01(string(buffer[:mLen])))
-
-	} else if strings.Contains(message, "AP10") { // AP10 = Alert?
-		fmt.Print(parseAP10(string(buffer[:mLen])))
-
-	}
-	//connection.Close()
 }
--- parsers.go
+++ parsers.go
@@ -1,13 +1,74 @@
 package main
 
-func parseAP00(msg string) string {
-	return msg
+import (
+	"fmt"
+	"strings"
+)
+
+type PreciseGPSData struct {
+	deviceimei      string
+	latitude        float64
+	longitude       float64
+	altitude        int
+	devicetime      string
+	speed           float64
+	Batterylevel    int
+	casefile_id     string
+	address         string
+	positioningmode string
+	tz              string
+	offender_name   string
+	offender_id     string
 }
 
-func parseAP01(msg string) string {
-	return msg
+func getIMEIFromAP00(msg string) string {
+	var data = strings.Split(msg, "'")[0]
+	return data[6:21]
 }
 
-func parseAP10(msg string) string {
-	return msg
+func getIMEIFromLK(msg string) string {
+	var data = strings.Split(msg, "'")[0]
+	return data[1:15]
+}
+
+func getJSONFromAP01(msg string) PreciseGPSData {
+	retObj := PreciseGPSData{
+		deviceimei:      "string",
+		latitude:        0.0,
+		longitude:       0.0,
+		altitude:        0,
+		devicetime:      "string",
+		speed:           0.0,
+		Batterylevel:    0,
+		casefile_id:     "string",
+		address:         "string",
+		positioningmode: "string",
+		tz:              "string",
+		offender_name:   "string",
+		offender_id:     "string",
+	}
+	return retObj
+}
+
+func getJSONFromAP10(msg string) PreciseGPSData {
+	retObj := PreciseGPSData{
+		deviceimei:      "string",
+		latitude:        0.0,
+		longitude:       0.0,
+		altitude:        0,
+		devicetime:      "string",
+		speed:           0.0,
+		Batterylevel:    0,
+		casefile_id:     "string",
+		address:         "string",
+		positioningmode: "string",
+		tz:              "string",
+		offender_name:   "string",
+		offender_id:     "string",
+	}
+	return retObj
+}
+
+func convertLatLon(latOrLon float64) {
+	fmt.Println("Convert: ", latOrLon)
 }
