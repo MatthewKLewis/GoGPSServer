d6f7bca4c5a2129fa6ef311ae1cea0dff7faf46b
MatthewKLewis <matthew.knight.lewis@gmail.com>
2022-12-01 10:35:42 -0500

    fixes for json?

--- parsers.go
+++ parsers.go
@@ -12,7 +12,7 @@ type PreciseGPSData struct {
 	Latitude          float64 `json:"latitude"`
 	Longitude         float64 `json:"longitude"`
 	Altitude          int     `json:"altitude"`
-	Devicetime        string  `json:"devicetime"`
+	DeviceTime        string  `json:"devicetime"`
 	Speed             float64 `json:"speed"`
 	BatteryLevel      int     `json:"Batterylevel"`
 	CasefileId        string  `json:"casefile_id"`
@@ -35,20 +35,20 @@ func getIMEIFromLK(msg string) string {
 // IWAP01301122V0000.0000N00000.0000E000.0201934000.0003900009700003,310,260,46136,11869187,AP1|16:8d:db:65:af:0f|-41&AP2|5c:5b:35:01:cd:d1|-43&AP3|3a:22:e2:a3:e1:07|-45&AP4|70:7d:b9:e1:95:c4|-47&AP5|70:7d:b9:e1:95:ce|-51#
 func getJSONFromAP01(msg string, deviceIMEI string) (PreciseGPSData, error) {
 	retObj := PreciseGPSData{
-		Deviceimei:          deviceIMEI,
-		latitude:            0.0,
-		longitude:           0.0,
-		altitude:            0,
-		devicetime:          "",
-		speed:               0.0,
-		Batterylevel:        0,
-		casefile_id:         "",
-		address:             "",
-		positioningmode:     "",
-		tz:                  "",
-		offender_name:       "",
-		offender_id:         "",
-		loc_message_content: "",
+		Deviceimei:        deviceIMEI,
+		Latitude:          0.0,
+		Longitude:         0.0,
+		Altitude:          0,
+		DeviceTime:        "",
+		Speed:             0.0,
+		BatteryLevel:      0,
+		CasefileId:        "",
+		Address:           "",
+		PositioningMode:   "",
+		Tz:                "",
+		OffenderName:      "",
+		OffenderId:        "",
+		LocMessageContent: "",
 	}
 
 	// if msg[12] != 'A' { // Valid packets have an 'A' (65) rather than a 'V' (86) at index 6
@@ -63,18 +63,18 @@ func getJSONFromAP01(msg string, deviceIMEI string) (PreciseGPSData, error) {
 		fmt.Println(err.Error())
 		return retObj, err
 	}
-	retObj.latitude = latDeg + latPoints/300
-	retObj.longitude = lonDeg + lonPoints/300
+	retObj.Latitude = latDeg + latPoints/300
+	retObj.Longitude = lonDeg + lonPoints/300
 
 	rssis := strings.Split(msg, "|")[1:]
 	for i := 0; i < len(rssis); i++ {
 		fmt.Println(rssis[i])
 		if i%2 == 0 {
-			retObj.loc_message_content += rssis[i] + ":"
+			retObj.LocMessageContent += rssis[i] + ":"
 		} else if i == len(rssis)-1 {
-			retObj.loc_message_content += strings.Split(rssis[i], "&")[0] + "#"
+			retObj.LocMessageContent += strings.Split(rssis[i], "&")[0] + "#"
 		} else {
-			retObj.loc_message_content += strings.Split(rssis[i], "&")[0] + ";"
+			retObj.LocMessageContent += strings.Split(rssis[i], "&")[0] + ";"
 		}
 	}
 	return retObj, nil
@@ -82,20 +82,20 @@ func getJSONFromAP01(msg string, deviceIMEI string) (PreciseGPSData, error) {
 
 func getJSONFromAP10(msg string, deviceIMEI string) (PreciseGPSData, error) {
 	retObj := PreciseGPSData{
-		deviceimei:          deviceIMEI,
-		latitude:            0.0,
-		longitude:           0.0,
-		altitude:            0,
-		devicetime:          "",
-		speed:               0.0,
-		Batterylevel:        0,
-		casefile_id:         "",
-		address:             "",
-		positioningmode:     "",
-		tz:                  "",
-		offender_name:       "",
-		offender_id:         "",
-		loc_message_content: "",
+		Deviceimei:        deviceIMEI,
+		Latitude:          0.0,
+		Longitude:         0.0,
+		Altitude:          0,
+		DeviceTime:        "",
+		Speed:             0.0,
+		BatteryLevel:      0,
+		CasefileId:        "",
+		Address:           "",
+		PositioningMode:   "",
+		Tz:                "",
+		OffenderName:      "",
+		OffenderId:        "",
+		LocMessageContent: "",
 	}
 
 	// if msg[12] != 'A' { // Valid packets have an 'A' (65) rather than a 'V' (86) at index 6
@@ -110,17 +110,19 @@ func getJSONFromAP10(msg string, deviceIMEI string) (PreciseGPSData, error) {
 		fmt.Println(err.Error())
 		return retObj, err
 	}
-	retObj.latitude = latDeg + latPoints/300
-	retObj.longitude = lonDeg + lonPoints/300
+	retObj.Latitude = latDeg + latPoints/300
+	retObj.Longitude = lonDeg + lonPoints/300
 	fmt.Println(latDeg, latPoints, lonDeg, lonPoints)
 
 	rssis := strings.Split(msg, "|")[1:]
 	for i := 0; i < len(rssis); i++ {
 		fmt.Println(rssis[i])
 		if i%2 == 0 {
-			retObj.loc_message_content += rssis[i] + ":"
+			retObj.LocMessageContent += rssis[i] + ":"
+		} else if i == len(rssis)-1 {
+			retObj.LocMessageContent += strings.Split(rssis[i], "&")[0] + "#"
 		} else {
-			retObj.loc_message_content += rssis[i] + ";"
+			retObj.LocMessageContent += strings.Split(rssis[i], "&")[0] + ";"
 		}
 	}
 
@@ -129,19 +131,20 @@ func getJSONFromAP10(msg string, deviceIMEI string) (PreciseGPSData, error) {
 
 func getJSONFromCUSTOMER(msg string, deviceIMEI string) (PreciseGPSData, error) {
 	retObj := PreciseGPSData{
-		deviceimei:      deviceIMEI,
-		latitude:        0.0,
-		longitude:       0.0,
-		altitude:        0,
-		devicetime:      "new go parser",
-		speed:           0.0,
-		Batterylevel:    0,
-		casefile_id:     "new go parser",
-		address:         "new go parser",
-		positioningmode: "new go parser",
-		tz:              "new go parser",
-		offender_name:   "new go parser",
-		offender_id:     "new go parser",
+		Deviceimei:        deviceIMEI,
+		Latitude:          0.0,
+		Longitude:         0.0,
+		Altitude:          0,
+		DeviceTime:        "",
+		Speed:             0.0,
+		BatteryLevel:      0,
+		CasefileId:        "",
+		Address:           "",
+		PositioningMode:   "",
+		Tz:                "",
+		OffenderName:      "",
+		OffenderId:        "",
+		LocMessageContent: "",
 	}
 
 	if msg[6] != 'A' {
